a6ce47ea16dfa80b049c4ef1eaf1d5cb76f1ed11
Franco Cordoba <francocordobafs@gmail.com>
2025-05-21 21:09:15 -0300

    arrows functions created

--- 00 - Ejercicios_varios/pokedex/javascript/index.js
+++ 00 - Ejercicios_varios/pokedex/javascript/index.js
@@ -1,6 +1,7 @@
 let pkmNumber = 1
 let pkmList = getPkmNames()
 let data
+let pressed = null
 
 // https://pokeapi.co/api/v2/pokemon/
 
@@ -10,7 +11,9 @@ document.body.innerHTML = `
             <div id="div_background">
                 <div id="div_screen">
                     <div id="first_screen">
-                        <img src="" alt="" id="sprite">
+                        <div id="sprite">
+                            <img src="" alt="" id="img_sprite">
+                        </div>
                         <div id="pokedex_seen-own">
                             <div>
                                 <p>SEEN</p>
@@ -30,7 +33,9 @@ document.body.innerHTML = `
                 <div id="buttons">
                     <div id="pokedex_controller">
                         <button id="up">↑</button>
-                        <button id="down">↓</button>
+                        <button id="down">↑</button>
+                        <button id="left">↑</button>
+                        <button id="right">↑</button>
                     </div>
                     <input type="text" placeholder="ingresar Nº" id="input_num-pokedex" hidden="true">
                 </div>
@@ -45,27 +50,113 @@ document.body.innerHTML = `
 // 2198	 ↘ 	South East Arrow
 // 2199	 ↙ 	South West Arrow
 
-let imgTag = document.getElementById("sprite")
+let imgTag = document.getElementById("img_sprite")
 let btnUp = document.getElementById("up")
 let btnDown = document.getElementById("down")
+let btnLeft = document.getElementById("left")
+let btnRight = document.getElementById("right")
 let inputNumPokedex = document.getElementById("input_num-pokedex")
 let pkdxNames = document.getElementById("pokedex_names")
 
 imgTag.src = renderImg(1)
 
-btnUp.addEventListener("click", () => {
-    pkmNumber > 1 ? pkmNumber-- : null
-    renderImg(pkmNumber)
-})
-btnDown.addEventListener("click", () => {
-    pkmNumber < 251 ? pkmNumber++ : null
-    renderImg(pkmNumber)
-})
+
+btnUp.addEventListener("mousedown", () => arrowKeys("up"))
+btnUp.addEventListener("mouseup", () => arrowKeys("stop"))
+
+
+btnDown.addEventListener("mousedown", () => arrowKeys("down"))
+btnDown.addEventListener("mouseup", () => arrowKeys("stop"))
+
+btnLeft.addEventListener("mousedown", () => arrowKeys("left"))
+btnLeft.addEventListener("mouseup", () => arrowKeys("stop"))
+
+btnRight.addEventListener("mousedown", () => arrowKeys("right"))
+btnRight.addEventListener("mouseup", () => arrowKeys("stop"))
+
 
 inputNumPokedex.addEventListener("change", (el) => {
     pkmNumber = el.target.value
     renderImg(el.target.value)
 })
+
+
+function arrowKeys(key, event) {
+
+    const leftKey = () => {
+        const doOnce = () => {
+            pkmNumber > 6 ? pkmNumber -= 6 : pkmNumber = 1
+            renderImg(pkmNumber)
+        };
+        doOnce()
+
+        pressed = setInterval(() => {
+            doOnce()
+        }, 300)
+    }
+
+    const rightKey = () => {
+        const doOnce = () => {
+            pkmNumber < 245 ? pkmNumber += 6 : pkmNumber = 251
+            renderImg(pkmNumber)
+        };
+        doOnce()
+
+        pressed = setInterval(() => {
+            doOnce()
+        }, 300)
+    }
+
+    const upKey = () => {
+        const doOnce = ()=>{
+            pkmNumber > 1 ? pkmNumber-- : null
+            renderImg(pkmNumber)
+        }
+        doOnce()
+
+        pressed = setInterval(()=>{
+            doOnce()
+        }, 300)
+
+    }
+
+    const downKey = () => {
+        const doOnce = () => {
+            pkmNumber < 251 ? pkmNumber++ : null
+            renderImg(pkmNumber)
+        }
+        doOnce()
+
+        pressed = setInterval(() => {
+            doOnce()
+        }, 300)
+    }
+
+    const stopFire = () => {
+        clearInterval(pressed)
+    }
+
+    switch (key) {
+        case "left":
+            leftKey()
+            break;
+        case "right":
+            rightKey()
+            break;
+        case "up":
+            upKey()
+            break;
+        case "down":
+            downKey()
+            break;
+        case "stop":
+            stopFire()
+            break;
+        default:
+            stopFire()
+            break;
+    }
+}
 
 function renderImg(id = 1) {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -109,7 +200,7 @@ function cursor(id = 1) {
         el.value === id ? el.id = "selected" : el.id = ""
 
         if (id > 7) {
-            el.style = `top: ${(id - 7) * -22.6}px`
+            el.style = `top: ${(id - 7) * -22}px`
         } else {
             el.style = ""
         }
