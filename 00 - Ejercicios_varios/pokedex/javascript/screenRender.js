2203e824150ab242d16864ad37e6c7099f628222
Franco Cordoba <francocordobafs@gmail.com>
2025-06-06 22:01:16 -0300

    before mod background

--- 00 - Ejercicios_varios/pokedex/javascript/screenRender.js
+++ 00 - Ejercicios_varios/pokedex/javascript/screenRender.js
@@ -6,31 +6,36 @@ let screenDisplayedModule = {}
 
 // https://pokeapi.co/api/v2/pokemon/
 
-function updateDisplay(newScreen){
+function updateDisplay(newScreen = 2) {
+
     display = newScreen
+
     switch (display) {
         case 1:
             screenDisplayedModule = { buttonFunction, MainScreenBase, renderScreen }
+            // console.log("case 1", display);
             break;
         case 2:
             screenDisplayedModule = { buttonFunction, MainScreenBase: MainScreenBase01, renderScreen }
+            // console.log("case 2", display);
             break;
         default:
             break;
     }
 }
 
-function getScreenDisplayed(){
+function getScreenDisplayed() {
     return screenDisplayedModule
 }
 
 
 function render(screenDisplayedModule) {
-    document.addEventListener("DOMContentLoaded", async () => {
+    // document.addEventListener("DOMContentLoaded",  async  () => {
         document.body.innerHTML = screenDisplayedModule.MainScreenBase
         const screen = screenDisplayedModule.renderScreen()
-        await screen.init()
-    })
+        /* await */ screen.init()
+        console.log("render", display);
+    // })
 }
 
 function clearDOM() {
